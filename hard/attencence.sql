100810a6c05c7b49d6c83d2a0da976721b4b2294
AmanDaewon <120003773+AmanDaewon@users.noreply.github.com>
2025-04-11 18:42:47 +0530

    Add files via upload

---
+++ hard/attencence.sql
@@ -0,0 +1,6 @@
+CREATE TABLE attendance (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    roll VARCHAR(10),
+    name VARCHAR(100),
+    status VARCHAR(20)
+);
---
+++ meduim/employees.sql
@@ -0,0 +1,5 @@
+CREATE TABLE employees (
+    id INT PRIMARY KEY,
+    name VARCHAR(100),
+    role VARCHAR(100)
+);
